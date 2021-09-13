6b136e72ce9f469d92893820c402c2c0c8cc637c
Natalia <maltseva@arrival.com>
2021-09-13 23:27:29 +0300

    basic project structure

---
+++ backend/main.py
@@ -0,0 +1,11 @@
+import os
+
+from flask import Flask
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+socketio = SocketIO(app)
+
+if __name__ == '__main__':
+    socketio.run(app)
